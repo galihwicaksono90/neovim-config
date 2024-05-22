302199070e12f9401f678f694d9c08469baab276
Galih Wicaksono <galihwicaksono90@gmail.com>
2024-05-22 14:01:08 +0700

    update neo-tree config

--- lua/options.lua
+++ lua/options.lua
@@ -5,6 +5,7 @@ vim.g.maplocalleader = ' '
 -- disable netrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
